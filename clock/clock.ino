48400b4008c0a5df02a955002432fd7feed789c5
Anais Moisy <anais.moisy@gmail.com>
2015-08-03 11:38:22 +0100

    merge Clock + Api

--- clock/clock.ino
+++ clock/clock.ino
@@ -14,16 +14,28 @@ Demo & Details @
 
 *************************************************************/
 
-int delaylegnth_speed = 50;  //speed to go to the correct 'time' when it is launched
-unsigned long delay_day1 = 5;  //I guess I will need only one delay and change it dynamicaly (see email to get how it is calculated)
-unsigned long delay_day2 = 5;
-unsigned long delay_night = 5;
+#include <Bridge.h>
+#include <HttpClient.h>
+#include <ArduinoJson.h>
+
+// Storage for JSON parsing
+#define MAX_JSON_STR 500
+char jsonStr[MAX_JSON_STR];
+const int BUFFER_SIZE = 500;
+int steps_day = 206;    //tricks to get a full 360 degree (I don't know why it is not doing it in 200 steps)
+int steps_night = 406; 
+String lat("55.9531");
+String lon("-3.1889");
+
+
+
+int delaylegnth_speed = 20;  //speed to go to the correct 'time' when it is launched
+
 int i=0;
-int steps_day1 = 206; //tricks to get a full 360 degree (I don't know why it is not doing it in 200 steps)
-int steps_night = 406;
-int steps_day2 = 606;
-int steps_current_time = 53; //number of steps to go to the correct 'time' when it is launched (see email to get how it is calculated)
 unsigned long start_time;
+unsigned long daySteps;
+unsigned long nightSteps;
+unsigned long stepsToInit;
 
 //for the LEDs
 #include <Wire.h>
@@ -61,12 +73,43 @@ void setup() {
   colorWipe(strip.Color(0, 0, 0), 1); // Red
   delay(500);
   
+  digitalWrite(13, LOW);
+  Bridge.begin();
+  Serial.begin(9600);
+  while(!Serial);
+  
 }
+
+
 
 void loop(){
   
+  // Get the sunrise and sunset times
+  uint32_t daySteps = 0;
+  uint32_t nightSteps = 0;
+  uint32_t stepsToInit = 0;
+  
+  if (getSunRiseSet(lat, lon, daySteps, nightSteps, stepsToInit))
+  {
+    Serial.print("daySteps ");
+    Serial.println(daySteps);
+    Serial.print("nightSteps ");
+    Serial.println(nightSteps);
+    Serial.print("stepsToInit ");
+    Serial.println(stepsToInit);
+  }
+  else
+  {
+    Serial.println("Failed to get times");
+  }  
+  
+  Serial.println("Setup complete");
+  
+
+  delay(5000);
+  
   //make needle turn fast to the right time 
-  while(i < steps_current_time){
+  while(i < stepsToInit){
     
     step1(delaylegnth_speed);
     step2(delaylegnth_speed);
@@ -83,30 +126,30 @@ void loop(){
   Serial.println(String(start_time));
   Serial.println(String(millis()));
   
-  //set the new step limit for day 1
-  steps_current_time = steps_day1;
+  //set the new step limit for day 
+  stepsToInit = steps_day;
   
-  //make needle turn right speed until sunset day1
-  while(i < steps_day1){
+  //make needle turn right speed until sunset day
+  while(i < steps_day){
       
       step1(1);
-      waitTime(delay_day1);
+      waitTime(daySteps);
       
       step2(1);
-      waitTime(delay_day1);
+      waitTime(daySteps);
       
       step3(1);
-      waitTime(delay_day1);
+      waitTime(daySteps);
       
       step4(1);
-      waitTime(delay_day1);
+      waitTime(daySteps);
   }
   
   Serial.println("I is: " + String(i));
   Serial.println("Day 1 is over!");
-  steps_current_time = steps_night;
+  stepsToInit = steps_night;
   
-  if (i==steps_day1){
+  if (i==steps_day){
   delay(5);
   colorWipe(strip.Color(255, 255, 255), 1); // Red
   delay(5);
@@ -116,16 +159,16 @@ void loop(){
   while(i < steps_night){
    
       step1(1);
-      waitTime(delay_night);
+      waitTime(nightSteps);
       
       step2(1);
-      waitTime(delay_night);
+      waitTime(nightSteps);
       
       step3(1);
-      waitTime(delay_night);
+      waitTime(nightSteps);
       
       step4(1);
-      waitTime(delay_night);
+      waitTime(nightSteps);
   }
   
   if (i==steps_night){
@@ -133,30 +176,6 @@ void loop(){
   colorWipe(strip.Color(0, 0, 0), 1); // Red
   delay(5);
   }
-  
-  //set the new step limit for day 2
-  Serial.println("I is: " + String(i));
-  Serial.println("Night 1 is over!");
-  steps_current_time = steps_day2;
-  
-  //make needle turn right speed until sunset day2
-  while(i < steps_day2){
-      
-      step1(1);
-      waitTime(delay_day2);
-      
-      step2(1);
-      waitTime(delay_day2);
-      
-      step3(1);
-      waitTime(delay_day2);
-      
-      step4(1);
-      waitTime(delay_day2);
-  }
-  
-  Serial.println("I is: " + String(i));
-  Serial.println("Day 2 is over!");
   
 }
 
@@ -167,8 +186,8 @@ boolean waitTime(unsigned long time)
   
   Serial.println("time day one is being called:");
   Serial.println(start_time);
-  Serial.println("Starttime + delay_day_1 is:");
-  Serial.println(start_time + delay_day1);
+  Serial.println("Starttime + delay_day is:");
+  Serial.println(start_time + daySteps);
   while ((start_time + time) > millis())
   {
     delay(5);
@@ -184,7 +203,7 @@ boolean waitTime(unsigned long time)
   
 void step1(int delayMilliseconds)
 {
- if (i < steps_current_time)
+ if (i < stepsToInit)
     {
     digitalWrite(9, LOW);  //ENABLE CH A
     digitalWrite(8, HIGH); //DISABLE CH B
@@ -200,7 +219,7 @@ void step1(int delayMilliseconds)
 
 void step2(int delayMilliseconds)
 {
-  if (i < steps_current_time)
+  if (i < stepsToInit)
     {
     digitalWrite(9, HIGH);  //DISABLE CH A
     digitalWrite(8, LOW); //ENABLE CH B
@@ -216,7 +235,7 @@ void step2(int delayMilliseconds)
 
 void step3(int delayMilliseconds)
 {
-  if (i < steps_current_time)
+  if (i < stepsToInit)
     {
     digitalWrite(9, LOW);  //ENABLE CH A
     digitalWrite(8, HIGH); //DISABLE CH B
@@ -232,7 +251,7 @@ void step3(int delayMilliseconds)
 
 void step4(int delayMilliseconds)
 {
-  if (i < steps_current_time)
+  if (i < stepsToInit)
     {
     digitalWrite(9, HIGH);  //DISABLE CH A
     digitalWrite(8, LOW); //ENABLE CH B
@@ -253,4 +272,55 @@ void colorWipe(uint32_t c, uint8_t wait) {
 	strip.show();
 	delay(wait);
   }
+}
+
+// Get the sunrise and sunset times from openweather.org
+boolean getSunRiseSet(String lat, String lon, uint32_t &daySteps, uint32_t &nightSteps, uint32_t &stepsToInit)
+{
+  // Connect to server
+  HttpClient client;
+
+  // Create a HTTP request:
+  String url = "http://anaismoisy.com/apps/clock/api.php?lat=" + lat + "&lng=" + lon + "&steps=" + steps_day;
+  Serial.println("Getting " + url);
+
+  client.get(url);
+
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  
+  // Get the JSON data
+  int j = 0;
+  while (client.available()) 
+  {
+    char c = client.read();
+    jsonStr[j++] = c;
+    Serial.print(c);
+    
+  }
+  jsonStr[j++] += '\0';
+  
+  int i = 0;
+  while(jsonStr[i])
+  {
+    Serial.print(jsonStr[i++]);
+  }
+  Serial.print("\0");
+  
+  // Parse the JSON data
+  StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(jsonStr);
+
+  if (!root.success()) 
+  {
+    Serial.println("parseObject() failed");
+    return false;
+  }
+
+  daySteps = root["daySteps"];
+  nightSteps = root["nightSteps"];
+  stepsToInit = root["stepsToInit"];
+
+  // Success
+  return true;
 }
