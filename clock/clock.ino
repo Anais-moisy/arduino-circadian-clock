702fd0664607c6f40aa6ef247da4392b00d5a3f0
Anais Moisy <anais.moisy@gmail.com>
2015-08-03 20:12:20 +0100

    Now getting setup from web service, night light disabled for now as it conflicts with the json library.

--- clock/clock.ino
+++ clock/clock.ino
@@ -13,6 +13,12 @@ Demo & Details @
 - http://anaismoisy.com/essay-cardican-clock/
 
 *************************************************************/
+//for the LEDs
+//#include <Wire.h>
+//#include <Adafruit_NeoPixel.h> 
+//#define NEOPIN 6  // define pins
+//#define BRIGHTNESS 64 // set max brightness 
+//Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, NEOPIN, NEO_GRB + NEO_KHZ800); // strip object
 
 #include <Bridge.h>
 #include <HttpClient.h>
@@ -22,34 +28,24 @@ Demo & Details @
 #define MAX_JSON_STR 500
 char jsonStr[MAX_JSON_STR];
 const int BUFFER_SIZE = 500;
-int steps_day = 206;    //tricks to get a full 360 degree (I don't know why it is not doing it in 200 steps)
-int steps_night = 406; 
+int steps_day = 206; //tricks to get a full 360 degree (I don't know why it is not doing it in 200 steps)
+int steps_night = 406;
 String lat("55.9531");
 String lon("-3.1889");
 
-
-
-int delaylegnth_speed = 20;  //speed to go to the correct 'time' when it is launched
-
+int delaylegnth_speed = 50;  //speed to go to the correct 'time' when it is launched
 int i=0;
 unsigned long start_time;
-unsigned long daySteps;
-unsigned long nightSteps;
-unsigned long stepsToInit;
-
-//for the LEDs
-#include <Wire.h>
-#include <Adafruit_NeoPixel.h> 
-#define NEOPIN 6  // define pins
-#define BRIGHTNESS 64 // set max brightness 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, NEOPIN, NEO_GRB + NEO_KHZ800); // strip object
-
-
-
-
+int steps_current_time;
 
 
 void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();
+  Serial.begin(9600);
+  //while(!Serial);
+  
   
   //establish motor direction toggle pins
   pinMode(12, OUTPUT); //CH A -- HIGH = forwards and LOW = backwards???
@@ -65,25 +61,16 @@ void setup() {
   Serial.println("Setup complete");
   
   //setup LEDs
-  Wire.begin();        	// Begin I2C
-  pinMode(NEOPIN, OUTPUT);
-  strip.begin();
-  strip.setBrightness(BRIGHTNESS); // set brightness
-  delay(500);
-  colorWipe(strip.Color(0, 0, 0), 1); // Red
-  delay(500);
-  
-  digitalWrite(13, LOW);
-  Bridge.begin();
-  Serial.begin(9600);
-  while(!Serial);
-  
+  //Wire.begin();        	// Begin I2C
+  //pinMode(NEOPIN, OUTPUT);
+  //strip.begin();
+  //strip.setBrightness(BRIGHTNESS); // set brightness
+  //delay(500);
+  //colorWipe(strip.Color(0, 0, 0), 1); // Red
+  //delay(500);
 }
 
-
-
-void loop(){
-  
+void loop() {
   // Get the sunrise and sunset times
   uint32_t daySteps = 0;
   uint32_t nightSteps = 0;
@@ -106,9 +93,10 @@ void loop(){
   Serial.println("Setup complete");
   
 
-  delay(5000);
+  delay(2000);
   
-  //make needle turn fast to the right time 
+  steps_current_time = stepsToInit;
+    //make needle turn fast to the right time 
   while(i < stepsToInit){
     
     step1(delaylegnth_speed);
@@ -118,18 +106,15 @@ void loop(){
   }
   
   Serial.println("Time was initialized");
-  Serial.println("I is: " + String(i));
-  
+  //Serial.println("I is: " + String(i));
   
   start_time = millis();
-  Serial.println("HEEEERREEE startime: ");
   Serial.println(String(start_time));
-  Serial.println(String(millis()));
   
-  //set the new step limit for day 
-  stepsToInit = steps_day;
+  //set the new step limit for day 1
+  steps_current_time = steps_day;
   
-  //make needle turn right speed until sunset day
+  //make needle turn right speed until sunset day1
   while(i < steps_day){
       
       step1(1);
@@ -145,15 +130,16 @@ void loop(){
       waitTime(daySteps);
   }
   
+  
   Serial.println("I is: " + String(i));
   Serial.println("Day 1 is over!");
-  stepsToInit = steps_night;
+  steps_current_time = steps_night;
   
-  if (i==steps_day){
-  delay(5);
-  colorWipe(strip.Color(255, 255, 255), 1); // Red
-  delay(5);
-  }
+  //if (i==steps_day1){
+  //delay(5);
+  //colorWipe(strip.Color(255, 255, 255), 1); // Red
+  //delay(5);
+  //}
   
   //make needle turn right speed until sunrise day2
   while(i < steps_night){
@@ -171,107 +157,14 @@ void loop(){
       waitTime(nightSteps);
   }
   
-  if (i==steps_night){
-  delay(5);
-  colorWipe(strip.Color(0, 0, 0), 1); // Red
-  delay(5);
-  }
+  //if (i==steps_night){
+  //delay(5);
+  //colorWipe(strip.Color(0, 0, 0), 1); // Red
+  //delay(5);
+  //}
   
-}
-
-
-//delay between steps for day1
-boolean waitTime(unsigned long time)
-{
   
-  Serial.println("time day one is being called:");
-  Serial.println(start_time);
-  Serial.println("Starttime + delay_day is:");
-  Serial.println(start_time + daySteps);
-  while ((start_time + time) > millis())
-  {
-    delay(5);
-  }
-  
-  Serial.println("Setting new Starttime to now:");
-  Serial.println(millis());
-    start_time = millis();
-    return true;
-}
-
-
-  
-void step1(int delayMilliseconds)
-{
- if (i < stepsToInit)
-    {
-    digitalWrite(9, LOW);  //ENABLE CH A
-    digitalWrite(8, HIGH); //DISABLE CH B
-  
-    digitalWrite(12, HIGH);   //Sets direction of CH A
-    analogWrite(3, 255);   //Moves CH A
-    Serial.println("step 1");
-    Serial.println("delay is: " + String(delayMilliseconds));
-    delay(delayMilliseconds);
-    i++;
-    } 
-}
-
-void step2(int delayMilliseconds)
-{
-  if (i < stepsToInit)
-    {
-    digitalWrite(9, HIGH);  //DISABLE CH A
-    digitalWrite(8, LOW); //ENABLE CH B
-  
-    digitalWrite(13, LOW);   //Sets direction of CH B
-    analogWrite(11, 255);   //Moves CH B
-    Serial.println("step 2");
-    Serial.println("delay is: " + String(delayMilliseconds));
-    delay(delayMilliseconds);
-    i++;
-    }
-}
-
-void step3(int delayMilliseconds)
-{
-  if (i < stepsToInit)
-    {
-    digitalWrite(9, LOW);  //ENABLE CH A
-    digitalWrite(8, HIGH); //DISABLE CH B
-  
-    digitalWrite(12, LOW);   //Sets direction of CH A
-    analogWrite(3, 255);   //Moves CH A
-    Serial.println("step 3");
-    Serial.println("delay is: " + String(delayMilliseconds));
-    delay(delayMilliseconds);
-    i++;
-    }
-}
-
-void step4(int delayMilliseconds)
-{
-  if (i < stepsToInit)
-    {
-    digitalWrite(9, HIGH);  //DISABLE CH A
-    digitalWrite(8, LOW); //ENABLE CH B
-  
-    digitalWrite(13, HIGH);   //Sets direction of CH B
-    analogWrite(11, 255);   //Moves CH B
-    Serial.println("step 4");
-    Serial.println("delay is: " + String(delayMilliseconds));
-    delay(delayMilliseconds);
-    i++;
-    }
-}
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-	strip.setPixelColor(i, c);
-	strip.show();
-	delay(wait);
-  }
+  i = 0;
 }
 
 // Get the sunrise and sunset times from openweather.org
@@ -320,7 +213,102 @@ boolean getSunRiseSet(String lat, String lon, uint32_t &daySteps, uint32_t &nigh
   daySteps = root["daySteps"];
   nightSteps = root["nightSteps"];
   stepsToInit = root["stepsToInit"];
-
+  
+  daySteps = daySteps;
+  nightSteps = nightSteps;
+  
   // Success
   return true;
 }
+
+//delay between steps
+boolean waitTime(unsigned long time)
+{
+  
+  //Serial.println("time day one is being called:");
+  //Serial.println(start_time);
+  //Serial.println("Starttime + delay_day_1 is:");
+  //Serial.println(start_time + daySteps);
+  while ((start_time + time) > millis())
+  {
+    delay(5);
+  }
+  
+  Serial.println("Setting new Starttime to now:");
+  Serial.println(millis());
+    start_time = millis();
+    return true;
+}
+  
+void step1(int delayMilliseconds)
+{
+ if (i < steps_current_time)
+    {
+    digitalWrite(9, LOW);  //ENABLE CH A
+    digitalWrite(8, HIGH); //DISABLE CH B
+  
+    digitalWrite(12, HIGH);   //Sets direction of CH A
+    analogWrite(3, 255);   //Moves CH A
+    Serial.println("step 1");
+    Serial.println("delay is: " + String(delayMilliseconds));
+    delay(delayMilliseconds);
+    i++;
+    } 
+}
+
+void step2(int delayMilliseconds)
+{
+  if (i < steps_current_time)
+    {
+    digitalWrite(9, HIGH);  //DISABLE CH A
+    digitalWrite(8, LOW); //ENABLE CH B
+  
+    digitalWrite(13, LOW);   //Sets direction of CH B
+    analogWrite(11, 255);   //Moves CH B
+    Serial.println("step 2");
+    Serial.println("delay is: " + String(delayMilliseconds));
+    delay(delayMilliseconds);
+    i++;
+    }
+}
+
+void step3(int delayMilliseconds)
+{
+  if (i < steps_current_time)
+    {
+    digitalWrite(9, LOW);  //ENABLE CH A
+    digitalWrite(8, HIGH); //DISABLE CH B
+  
+    digitalWrite(12, LOW);   //Sets direction of CH A
+    analogWrite(3, 255);   //Moves CH A
+    Serial.println("step 3");
+    Serial.println("delay is: " + String(delayMilliseconds));
+    delay(delayMilliseconds);
+    i++;
+    }
+}
+
+void step4(int delayMilliseconds)
+{
+  if (i < steps_current_time)
+    {
+    digitalWrite(9, HIGH);  //DISABLE CH A
+    digitalWrite(8, LOW); //ENABLE CH B
+  
+    digitalWrite(13, HIGH);   //Sets direction of CH B
+    analogWrite(11, 255);   //Moves CH B
+    Serial.println("step 4");
+    Serial.println("delay is: " + String(delayMilliseconds));
+    delay(delayMilliseconds);
+    i++;
+    }
+}
+
+// Fill the dots one after the other with a color
+//void colorWipe(uint32_t c, uint8_t wait) {
+//  for(uint16_t i=0; i<strip.numPixels(); i++) {
+//	strip.setPixelColor(i, c);
+//	strip.show();
+//	delay(wait);
+//  }
+//}
